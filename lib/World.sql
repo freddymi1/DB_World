83877ef1142b61f3f7340a6f164017a0fd0eef18
freddymi1 <michelfreddy1992@gmail.com>
2020-08-31 09:45:17 +0300

    Modif

--- lib/World.sql
+++ lib/World.sql
@@ -19,8 +19,8 @@ INSERT INTO personne (id, nom, prenom, age, taille, poids) VALUES(5, 'Kiala', 'P
 ALTER TABLE personne
 ADD COLUMN couleur_preferer TEXT
 
-INSERT INTO personne (id, nom, prenom, age, taille, poids, couleur_preferer) VALUES(1, 'Rakoto', 'Be Nify', 10, '1,50', '70,0', 'rouge');
-INSERT INTO personne (id, nom, prenom, age, taille, poids, couleur_preferer) VALUES(2, 'Lava', 'Rapeto', 25, '2,0', '30,0', 'vert');
-INSERT INTO personne (id, nom, prenom, age, taille, poids, couleur_preferer) VALUES(3, 'Ba', 'Lita', 7, '1,0', '20,5', 'jaune');
-INSERT INTO personne (id, nom, prenom, age, taille, poids, couleur_preferer) VALUES(4, 'Kiala', 'manjakely', 100, '1,68', '45,7', 'violet');
-INSERT INTO personne (id, nom, prenom, age, taille, poids, couleur_preferer) VALUES(5, 'Kiala', 'Pota', 37, '0,8', '50,0', 'grise');
+UPDATE personne SET nom='Rakoto', prenom='Be Nify', age=10, taille='1,50', poids='70,0', couleur_preferer='rouge' WHERE id=1;
+UPDATE personne SET nom='Lava', prenom='Rapeto', age=25, taille='2,0', poids='30,0', couleur_preferer='vert' WHERE id=2;
+UPDATE personne SET nom='Ba', prenom='Lita', age=7, taille='1,0', poids='20,5', couleur_preferer='jaune' WHERE id=3;
+UPDATE personne SET nom='Kiala', prenom='manjakely', age=100, taille='1,68', poids='45,7', couleur_preferer='violet' WHERE id=4;
+UPDATE personne SET nom='Kioala', prenom='Pota', age=37, taille='0,8', poids='50,0', couleur_preferer='grise' WHERE id=5;
